a545ce9f26ac732d2f034acf4cace9cdbb36a2c7
GonzaFerreyra.21 <gonzalo41092@gmail.com>
2022-08-28 14:08:54 -0300

    Estados semideclarativos

--- src/UseState.js
+++ src/UseState.js
@@ -7,6 +7,8 @@ function UseState({ name }) {
     value: "",
     error:false,
     loading:false,
+    deleted:false,
+    confirmed: false,
  }); 
 
     console.log(state)
@@ -17,12 +19,13 @@ function UseState({ name }) {
        if(!!state.loading){
         setTimeout(()=>{
             console.log("Haciendo la validación")
-            if(state.value !== SEGURITY_CODE){
+            if(state.value === SEGURITY_CODE){
                 setState({
                     ...state,
                     error:false,
                     loading: false,
-                })
+                    confirmed: true,
+                });
             }else {
                 setState({
                     ...state,
@@ -40,7 +43,8 @@ function UseState({ name }) {
 
     }, [state.loading]);
 
-    return (
+    if(!state.deleted && !state.confirmed){ 
+        return (
         <div>
             <h2>Eliminar {name}</h2>
             <p>Por favor, escriba el codigo de seguridad</p>
@@ -48,7 +52,7 @@ function UseState({ name }) {
             {(state.error && !state.loading) && (
                 <p>Error: el código es incorrecto</p>
             )}
-             {state.loading && (
+            {state.loading && (
                 <p>Cargando...</p>
             )}
             <input 
@@ -71,7 +75,54 @@ function UseState({ name }) {
             }
             >Comprobar</button>
         </div>
-    );  
+        );  
+    }else if(!!state.confirmed && !state.deleted){
+        return(
+            <React.Fragment>
+                <p>Pedimos confirmación. ¿Tas seguro?</p>
+
+                <button
+                onClick={()=>{
+                    setState({
+                        ...state,
+                        deleted:true,
+                    });
+                }}
+                >
+                    Sí, eliminar
+                </button>
+                <button
+                onClick={()=>{
+                    setState({
+                        ...state,
+                        confirmed:false,
+                        value:"",
+                    });
+                }}
+                >
+                    No, me arrepentí
+                </button>
+            </React.Fragment>
+        );
+    }else {
+        return(
+            <React.Fragment>
+                <p>Elminado con exito</p>
+                <button
+                onClick={()=>{
+                    setState({
+                        ...state,
+                        confirmed:false,
+                        deleted:false,
+                        value:"",
+                    });
+                }}
+                >
+                    Resetear, volver atrás
+                </button>
+            </React.Fragment>
+        );
+    }     
 }
 // Nota:  "&&" es un condicional que se traduce como un entonces
 export { UseState };
